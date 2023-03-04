596d8959dba138d24a3ccbfd6e1b570b5a6ffc04
Aryodjat1 <aryodjati5@gmail.com>
2023-03-04 23:14:08 +0700

    fix some layout and add transaction fragment

--- app/src/main/java/com/readthefuckingmanual/fuckukk/ui/fragments/cashier/history/FragmentHistory.kt
+++ app/src/main/java/com/readthefuckingmanual/fuckukk/ui/fragments/cashier/history/FragmentHistory.kt
@@ -1,5 +1,6 @@
 package com.readthefuckingmanual.fuckukk.ui.fragments.cashier.history
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,14 +8,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.readthefuckingmanual.fuckukk.R
+import com.readthefuckingmanual.fuckukk.data.source.preferences.UserPreferences
 import com.readthefuckingmanual.fuckukk.databinding.FragmentHistoryBinding
 import com.readthefuckingmanual.fuckukk.databinding.FragmentMenuBinding
+import com.readthefuckingmanual.fuckukk.ui.activities.login.LoginActivity
 
 class FragmentHistory : Fragment() {
 
     private var binding : FragmentHistoryBinding? = null
     private var rvHistoryAdapter : ListHistoryAdapter? = null
 
+    private val userPreference by lazy {
+        UserPreferences(requireContext())
+    }
+
+    private val userToken by lazy {
+        userPreference.getSession().token
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         rvHistoryAdapter = ListHistoryAdapter()
@@ -43,6 +53,15 @@ class FragmentHistory : Fragment() {
 
     fun observeSelectedHistory(){
         // TODO:
+    }
+
+    fun setupBtnLogout(){
+        binding?.btnLogoutCashierHistory?.setOnClickListener {
+            userPreference.deleteSession()
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
     }
 
     companion object {
