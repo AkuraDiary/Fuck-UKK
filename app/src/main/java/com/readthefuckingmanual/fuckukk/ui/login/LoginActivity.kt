08010616c1d79accf58d2d894e7877f53ab32ad4
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2023-03-04 19:01:02 +0700

    harusnya login sudah

--- app/src/main/java/com/readthefuckingmanual/fuckukk/ui/login/LoginActivity.kt
+++ app/src/main/java/com/readthefuckingmanual/fuckukk/ui/login/LoginActivity.kt
@@ -4,48 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.readthefuckingmanual.fuckukk.R
+import com.readthefuckingmanual.fuckukk.data.model.UserModel
+import com.readthefuckingmanual.fuckukk.data.repository.AuthRepository
+import com.readthefuckingmanual.fuckukk.data.source.preferences.UserPreferences
 import com.readthefuckingmanual.fuckukk.databinding.ActivityLoginBinding
 import com.readthefuckingmanual.fuckukk.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private var binding : ActivityLoginBinding? = null
+    private val userPreferences by lazy { UserPreferences(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        // TODO: check logged in user
-//        if (!(userPreferences.getSession())?.user_id.isNullOrEmpty()){
-//            moveTOMainActivity()
-//        }
+//         check logged in user
+        if (!(userPreferences.getSession()).token.isNullOrEmpty()){
+            moveTOMainActivity()
+        }
+        setupInput()
 
-//        setupInput()
-//        observeLoginResult()
     }
 
-//    TODO: Repository Missing
-//    fun observeLoginResult(){
-//        Repository.userResponse.observe(this){
-//            if (it.user_id?.isNotEmpty()!!){
-//                userPreferences.saveSession(it.name!!, it.user_id!!)
-//                moveTOMainActivity()
-//            }
-//        }
-//    }
+    fun setupInput(){
 
-//    TODO: Repository Missing
-//    fun setupInput(){
-//
-//        binding?.btnLogin?.setOnClickListener{
-//            //DO LOGIN HERE
-//            if (formisvalid()){
-//                Repository.doLogin(
-//                    binding?.edtUsername?.text.toString(),
-//                    binding?.edtPassword?.text.toString()
-//                )
-//            }
-//        }
-//    }
+        binding?.btnLogin?.setOnClickListener{
+            //DO LOGIN HERE
+            if (formisvalid()){
+                AuthRepository.doLogin(
+                    binding?.edtUsername?.text.toString(),
+                    binding?.edtPassword?.text.toString()
+                ).observe(this){
+                    if (it != null){
+                        userPreferences.saveSession(it)
+                        moveTOMainActivity()
+                    }
+                }
+            }
+        }
+    }
 
     fun formisvalid() : Boolean{
         when{
--- app/src/main/java/com/readthefuckingmanual/fuckukk/ui/main/MainActivity.kt
+++ app/src/main/java/com/readthefuckingmanual/fuckukk/ui/main/MainActivity.kt
@@ -3,14 +3,21 @@ package com.readthefuckingmanual.fuckukk.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.readthefuckingmanual.fuckukk.R
+import com.readthefuckingmanual.fuckukk.data.source.preferences.UserPreferences
 import com.readthefuckingmanual.fuckukk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding : ActivityMainBinding? = null
+    private val userPreferences by lazy { UserPreferences(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        greet()
+    }
+
+    fun greet(){
+        binding?.textView?.text = "Hello ${userPreferences.getSession().username}"
     }
 
     override fun onDestroy() {
