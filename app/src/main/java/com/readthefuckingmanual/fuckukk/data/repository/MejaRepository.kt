baab85aa79d87f92cc1ad3e5d60e5bf579bed159
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2023-03-04 21:29:27 +0700

    meja repository and datasource

--- app/src/main/java/com/readthefuckingmanual/fuckukk/data/repository/MejaRepository.kt
+++ app/src/main/java/com/readthefuckingmanual/fuckukk/data/repository/MejaRepository.kt
@@ -1,5 +1,19 @@
 package com.readthefuckingmanual.fuckukk.data.repository
 
+import androidx.lifecycle.MutableLiveData
+import com.readthefuckingmanual.fuckukk.data.model.meja.ListMejaResponse
+import com.readthefuckingmanual.fuckukk.data.model.meja.MejaModel
+import com.readthefuckingmanual.fuckukk.data.source.remote.datasource.MejaRemoteDataSource
+
 object MejaRepository {
-    //TODO
+
+    fun getListMeja(token : String) : MutableLiveData<ListMejaResponse?> {
+        val mejaList = MutableLiveData<ListMejaResponse?>()
+        MejaRemoteDataSource.apply {
+            getListMeja(token)
+            return mejaList
+        }
+
+
+    }
 }
--- app/src/main/java/com/readthefuckingmanual/fuckukk/data/source/remote/datasource/MejaRemoteDataSource.kt
+++ app/src/main/java/com/readthefuckingmanual/fuckukk/data/source/remote/datasource/MejaRemoteDataSource.kt
@@ -1,6 +1,33 @@
 package com.readthefuckingmanual.fuckukk.data.source.remote.datasource
 
+import androidx.lifecycle.MutableLiveData
+import com.readthefuckingmanual.fuckukk.data.model.meja.ListMejaResponse
+import com.readthefuckingmanual.fuckukk.data.source.remote.retrofit.RetrofitConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 object MejaRemoteDataSource {
-    //TODO
+
+    private val _mejaList : MutableLiveData<ListMejaResponse?> = MutableLiveData()
+    val mejaList : MutableLiveData<ListMejaResponse?> = _mejaList
+
+    fun getListMeja(token : String){
+        RetrofitConfig.ApiService.getAllMeja(token)?.enqueue(
+            object : Callback<ListMejaResponse?> {
+                override fun onResponse(
+                    call: Call<ListMejaResponse?>,
+                    response: Response<ListMejaResponse?>
+                ) {
+                    if (response.isSuccessful){
+                        _mejaList.value = response.body()
+                    }
+                }
+
+                override fun onFailure(call: retrofit2.Call<ListMejaResponse?>, t: Throwable) {
+                    _mejaList.postValue(null)
+                }
+            })
+    }
 
 }
