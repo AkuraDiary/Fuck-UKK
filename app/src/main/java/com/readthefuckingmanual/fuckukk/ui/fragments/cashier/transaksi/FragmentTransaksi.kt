90633fd045ba351aedf0caf1caeaf7a2953fcd6b
Aryodjat1 <aryodjati5@gmail.com>
2023-03-05 09:43:20 +0700

    todo spinner meja

--- app/src/main/java/com/readthefuckingmanual/fuckukk/ui/fragments/cashier/transaksi/FragmentTransaksi.kt
+++ app/src/main/java/com/readthefuckingmanual/fuckukk/ui/fragments/cashier/transaksi/FragmentTransaksi.kt
@@ -5,10 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.readthefuckingmanual.fuckukk.R
+import com.readthefuckingmanual.fuckukk.data.model.meja.MejaModel
+import com.readthefuckingmanual.fuckukk.data.repository.MejaRepository
+import com.readthefuckingmanual.fuckukk.data.source.preferences.UserPreferences
 
 class FragmentTransaksi : Fragment() {
 
+    private val userPreference by lazy {
+        UserPreferences(requireContext())
+    }
+
+    private val userToken by lazy {
+        userPreference.getSession().token
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         }
@@ -20,6 +32,44 @@ class FragmentTransaksi : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_transaksi, container, false)
+    }
+
+    fun setupSpinner(){
+        MejaRepository.getListMeja(userToken!!).observe(viewLifecycleOwner) {listMeja ->
+
+            val listMejaAvailable : ArrayList<MejaModel?>? = listMeja?.values as ArrayList<MejaModel?>?
+            val adapter = ArrayAdapter(requireContext(), R.layout.item_spinner_meja, listMeja!!.map {
+                it?.name
+            })
+            adapter.setDropDownViewResource(R.layout.item_spinner_layout)
+            binding?.apply {
+                .adapter = adapter
+                edtIndustriUsahaSpinner.onItemSelectedListener = object :
+                    AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        p0: AdapterView<*>?,
+                        p1: View?,
+                        p2: Int,
+                        p3: Long
+                    ) {
+                        val selection = edtIndustriUsahaSpinner.selectedItem
+                        val selectedIdIndustry = listIndustry?.first {
+                            it?.name == selection
+                        }?.id
+                        if (selectedIdIndustry != null) {
+                            usahaIndustri = selectedIdIndustry
+                        }
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                    }
+
+                }
+
+            }
+
+        }
     }
 
     companion object {
